a4f49a7e8790c5f07efc446f869879bd404a132c
janbermet <81546071+janbermet@users.noreply.github.com>
2021-11-19 09:47:09 +0600

    Update portfolyo.php

--- portfolyo.php
+++ portfolyo.php
@@ -3,21 +3,28 @@
 <body style="background-image: url(https://i.pinimg.com/originals/9c/c4/f1/9cc4f1b9d3a60c3432ef20dd4916a517.jpg);">
 <meta name="viewport" content="width=device-width, initial-scale=1">></body>
 <h1 style="background-color:yellow ;"align="center">HELLO GUYS</h1>
-<img src="image/42cb46ff-b2b1-4130-b413-2f4bb5fcbb80.jpeg" width="500" align="right" alt="">
+<img src="image....jpeg" width="500" align="right" alt="">
 <h1 style="color: blue; width: 15;"> 
- <p>Jan Bermet</p> 
- <p> age: 18</p>
- <p> status: student</p>   
- <p> University: AIU</p>
-  <p>Group:CSC-19</p>        
-  <P>Adivasior:Mevlut Bey</P>                   
- </h1>
+<p>Uzenova Bermet</p>
+<p> Age: 18</p>
+<p> Status: student</p>
+<p> University: AIU</p>
+<p>Group:CSC-19</p>
+<P>Adivasior:Mevlut Bey</P>
+<p>Home address: st.Kalyk-Akieva 111A</p>
+<h2>Phone number: 0999 123 150</h2>
+<div align='center'>
+<a href="https://vm.tiktok.com/ZSekpJFY6/"> <img src="https://cdn.worldvectorlogo.com/logos/tiktok-logo-2--1.svg" width="48" alt=""></a>
+<a href="https://instagram.com/cristiano?utm_medium=copy_link "> <img src="https://cdn.worldvectorlogo.com/logos/instagram-2016-5.svg" width="48" alt=""></a>
+<a href="https://wa.me/<0999123150>"> <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/WhatsApp_icon.png" width="48" alt=""></a>
+</div>
+</h1>
+
 <style>
 img {
 border-radius: 50%;
 
 }
-
 
 </style>
 </head >
