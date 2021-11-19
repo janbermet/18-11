50c6342c4c3a3ef89111957dd52107126d8b6049
janbermet <81546071+janbermet@users.noreply.github.com>
2021-11-19 09:58:04 +0600

    Update portfolyo.php

--- portfolyo.php
+++ portfolyo.php
@@ -1,9 +1,10 @@
 <html>
 <head>
+<title>Bermet</title>
 <body style="background-image: url(https://i.pinimg.com/originals/9c/c4/f1/9cc4f1b9d3a60c3432ef20dd4916a517.jpg);">
 <meta name="viewport" content="width=device-width, initial-scale=1">></body>
 <h1 style="background-color:yellow ;"align="center">HELLO GUYS</h1>
-<img src="image....jpeg" width="500" align="right" alt="">
+<img src="42cb46ff-b2b1-4130-b413-2f4bb5fcbb80.jpeg" width="500" align="right" alt="">
 <h1 style="color: blue; width: 15;"> 
 <p>Uzenova Bermet</p>
 <p> Age: 18</p>
